17c82aa198b02979f24160e07b0dce7e633f5f2d
Marcello Henrique <marcellohenrique426@gmail.com>
2025-05-21 08:20:10 -0300

    Update config.py

--- config.py
+++ config.py
@@ -16,5 +16,6 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Pasta para uploads, se estiver usando
-    UPLOAD_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploads')
+    UPLOAD_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static' 'uploads')
+    
     
